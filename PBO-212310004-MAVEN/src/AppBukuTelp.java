4368e94101e5bdf5eb3bef646640e4504f27d471
MuhamadAgus <113649505+MhmdAgus21@users.noreply.github.com>
2023-01-01 23:01:11 +0700

    Update AppBukuTelp.java

--- PBO-212310004-MAVEN/src/AppBukuTelp.java
+++ PBO-212310004-MAVEN/src/AppBukuTelp.java
@@ -1,39 +1,47 @@
 package com.ibik.pbo.praktikum;
 
+import javax.swing.JOptionPane;
 import java.util.ArrayList;
 
-import javax.swing.JOptionPane;
-
 public class AppBukuTelp {
+    
+    public static ArrayList<String> table;
+
     public static void main(String[] args) {
-        ArrayList<String> fieldName = new ArrayList<String>();
-        fieldName.add("Nama Lengkap");
-        fieldName.add("Alamat");
-        fieldName.add("No. Telp");
+        AppBukuTelp mainApps = new AppBukuTelp();
 
-        ArrayList<ArrayList<String>> contact = new ArrayList<ArrayList<String>>();
+        table = new ArrayList<String>();
+        table.add("Nama Lengkap");
+        table.add("Alamat");
+        table.add("No. Telp");
+        
+        ArrayList<ArrayList<String>> data_tabel = new ArrayList<ArrayList<String>>();
 
-        for (int i = 0; i < fieldName.size(); i++) {
-            contact.add(new ArrayList<String>());
-            for (int j = 0; j < fieldName.size(); j++) {
-                String value = JOptionPane.showInputDialog(null, "Masukkan " + fieldName.get(j) + ":",
-                        "Data ke - " + (i + 1), JOptionPane.QUESTION_MESSAGE);
-                contact.get(i).add(j, value);
+        for(int i = 0; i < table.size(); i++) {
+            data_tabel.add(new ArrayList<String>());
+            for(int j = 0; j < table.size(); j++) {
+                String values = JOptionPane.showInputDialog(null,
+                                "Masukkan " + table.get(j) + ":",
+                                "Data ke - " + i,
+                                JOptionPane.QUESTION_MESSAGE);
+                data_tabel.get(i).add(j, values);
             }
         }
 
-        String output = "";
+        String output = mainApps.PrintValues(data_tabel);
 
-        for (int i = 0; i < fieldName.size(); i++) {
-            output += "Data ke - " + (i + 1) + "\n";
-            for (int j = 0; j < fieldName.size(); j++) {
-                output += fieldName.get(j) + ": " + contact.get(i).get(j) + "\n";
+        JOptionPane.showMessageDialog(null, output, "Output Data", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private String PrintValues(ArrayList<ArrayList<String>> val) {
+        String output = "";
+        for(int i = 0; i < val.size(); i++) {
+            for(int j = 0; j < val.size(); j++) {
+                output += table.get(j) + ": " + val.get(i).get(j) + "\n";
             }
             output += "\n";
         }
-
-        JOptionPane.showMessageDialog(null, output, "Data", JOptionPane.INFORMATION_MESSAGE);
-
-        System.out.println(contact);
+        return output;
     }
+
 }
