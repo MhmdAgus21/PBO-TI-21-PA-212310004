0553f571fc71ac0edf82d5e10512d10c370658a5
MuhamadAgus <113649505+MhmdAgus21@users.noreply.github.com>
2023-01-01 23:00:47 +0700

    Update AppLogistik.java

--- PBO-212310004-MAVEN/src/AppLogistik.java
+++ PBO-212310004-MAVEN/src/AppLogistik.java
@@ -1,149 +1,162 @@
 package com.ibik.pbo.praktikum;
-import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
+import java.util.ArrayList;
 
 public class AppLogistik {
-	    public static ArrayList<String> order = new ArrayList<String>();
-	    public static boolean isStop = false;
-	    public static boolean validation = true;
 
-	    public static void main(String[] args) {
+    public static ArrayList<String> pesanan = new ArrayList<String>();
+    public static AppLogistik mainApps = new AppLogistik();
+    public static final String regex_angka = "[0-9]+";
+    public static boolean is_exit = false;
+    
+    public static void main(String[] args) {
+        String[] menus = {"Input Pesanan", "Lacak Pesanan"};
+        String teks_menus = "Pilih menu di bawah ini:\n";
 
-	        mainloop: do {
-	            try {
-	                String menu_label = "Menu : \n";
-	                menu_label += "1. Cek Ongkir\n";
-	                menu_label += "2. Cek Resi\n";
-	                menu_label += "3. Keluar\n\n";
-	                menu_label += "Pilih menu: ";
+        for(int i = 0; i < menus.length; i++) {
+            teks_menus += (i + 1) + ". " + menus[i] + "\n";
+        }
 
-	                String value = JOptionPane.showInputDialog(null, menu_label, "Menu",
-	                        JOptionPane.QUESTION_MESSAGE);
+        while(!is_exit) {
+            String pilihan_menu = JOptionPane.showInputDialog(null, teks_menus, "Aplikasi agusse Expedition", JOptionPane.QUESTION_MESSAGE);
+            
+            if((pilihan_menu != null) && (pilihan_menu.matches(regex_angka))) {
+                switch(Integer.parseInt(pilihan_menu)) {
+                    case 1:
+                        mainApps.InputPesanan();
+                    break;
 
-	                if (value == null) {
-	                    int exit = JOptionPane.showConfirmDialog(null, "Apakah Anda ingin keluar?", "Konfirmasi",
-	                            JOptionPane.YES_NO_OPTION);
-	                    if (exit == 0) {
-	                        JOptionPane.showMessageDialog(null, "Terima Kasih!", "Agusse Expedition",
-	                                JOptionPane.INFORMATION_MESSAGE);
-	                        break;
-	                    } else {
-	                        continue;
-	                    }
-	                } else if (value.isEmpty()) {
-	                    JOptionPane.showMessageDialog(null, "Mohon Masukkan Kode!", "Alert!", JOptionPane.ERROR_MESSAGE);
-	                    continue;
-	                }
+                    case 2:
+                        mainApps.LacakPesanan();
+                    break;
 
-	                int menu = Integer.parseInt(value);
+                    default:
+                        JOptionPane.showMessageDialog(null, "Menu tidak ada!", "PERINGATAN", JOptionPane.ERROR_MESSAGE);
+                }
+            } else if((pilihan_menu != null) && (!pilihan_menu.matches(regex_angka))) {
+                JOptionPane.showMessageDialog(null, "Inputan harus angka!", "PERINGATAN", JOptionPane.ERROR_MESSAGE);
+            } else {
+                mainApps.ExitApps("");
+            }
 
-	                switch (menu) {
-	                    case 1:
-	                        order.clear();
-	                        String field[] = { "Berat Barang (gr)", "Jarak Tempuh (km)" };
+        }
+    }
 
-	                        for (int h = 0; h < field.length; h++) {
-	                            String values = JOptionPane.showInputDialog(null, "Masukkan " + field[h],
-	                                    "Cek Ongkir",
-	                                    JOptionPane.QUESTION_MESSAGE);
-	                            if (values == null) {
-	                                int choose = JOptionPane.showConfirmDialog(null, "Apakah Anda Ingin Keluar?", "Keluar",
-	                                        JOptionPane.YES_NO_OPTION);
+    private void InputPesanan() {
+        final String[] title_pesanan = {"Berat Barang (gr)", "Jarak Barang (km)"};
+        boolean is_exit_input_pesanan = false;
+        int i = 0;
 
-	                                if (choose == 0) {
-	                                    isStop = true;
-	                                    JOptionPane.showMessageDialog(null, "Terima Kasih!", "XYZ Expedition",
-	                                            JOptionPane.INFORMATION_MESSAGE);
-	                                    break mainloop;
-	                                } else if (choose == 1) {
-	                                    isStop = false;
-	                                    h--;
-	                                }
-	                            } else if (values.isEmpty()) {
-	                                JOptionPane.showMessageDialog(null, "Harap Masukkan Data!", "Alert!",
-	                                        JOptionPane.ERROR_MESSAGE);
-	                                h--;
-	                            } else {
-	                                order.add(values);
-	                            }
-	                        }
+        while(!is_exit_input_pesanan) {
+            while(i < title_pesanan.length) {
+                String values = JOptionPane.showInputDialog(null,
+                                "Masukkan " + title_pesanan[i] + ":",
+                                "Input " + title_pesanan[i],
+                                JOptionPane.INFORMATION_MESSAGE);
 
-	                        int hargaOngkir = ongkir(order.get(0), order.get(1));
-	                        order.add(Integer.toString(hargaOngkir));
+                if((values != null) && (values.matches(regex_angka))) {
+                    pesanan.add(values);
+                    i++;
+                    if(i == 2) { mainApps.CetakResi(); }
+                } else if((values != null) && (!values.matches(regex_angka))) {
+                    JOptionPane.showMessageDialog(null, "Inputan harus angka!", "PERINGATAN", JOptionPane.ERROR_MESSAGE);
+                    i = 0;
+                } else {
+                    int pilihan = JOptionPane.showConfirmDialog(null, 
+                                  "Apakah Anda ingin keluar ke menu utama?",
+                                  "Konfirmasi",
+                                  JOptionPane.YES_NO_OPTION,
+                                  JOptionPane.WARNING_MESSAGE);
 
-	                        String noResi = "agusse-0001";
-	                        order.add(noResi);
+                    if(pilihan == JOptionPane.YES_OPTION) {
+                        i = 2;
+                        is_exit_input_pesanan = true;
+                    } else {
+                        is_exit_input_pesanan = false;
+                        i = 0;
+                    }
 
-	                        String output = "";
-	                        output += "Total ongkir : " + hargaOngkir + "\n";
-	                        output += "Resi : " + order.get(3);
+                }
 
-	                        JOptionPane.showMessageDialog(null, output, "Cek Ongkir",
-	                                JOptionPane.INFORMATION_MESSAGE);
-	                        break;
+            }
+            is_exit_input_pesanan = true;
+        }
+    }
 
-	                    case 2:
-	                        cekResi();
-	                        break;
+    private void LacakPesanan() {
+        boolean is_exit_lacak_pesanan = false;
+        int i = 0;
 
-	                    case 3:
-	                        int choose = JOptionPane.showConfirmDialog(null, "Apakah Anda Ingin Keluar?", "Keluar",
-	                                JOptionPane.YES_NO_OPTION);
+        if(pesanan.size() > i) {
+            while(!is_exit_lacak_pesanan) {
+                String input_resi = JOptionPane.showInputDialog(null, "Masukkan nomor resi");
+                
+                if((input_resi != null) && (input_resi.equals(pesanan.get(3)))) {
+                    mainApps.CetakResi();
+                    is_exit_lacak_pesanan = true;
+                } else if((input_resi != null) && (!input_resi.equals(pesanan.get(3)))) {
+                    JOptionPane.showMessageDialog(null, "Nomor Resi tidak terdaftar!", "PERINGATAN", JOptionPane.ERROR_MESSAGE);
+                    i = 0;
+                } else {
+                    int pilihan = JOptionPane.showConfirmDialog(null, 
+                                  "Apakah Anda ingin keluar ke menu utama?",
+                                  "Konfirmasi",
+                                  JOptionPane.YES_NO_OPTION,
+                                  JOptionPane.WARNING_MESSAGE);
 
-	                        if (choose == 0) {
-	                            isStop = true;
-	                            JOptionPane.showMessageDialog(null, "Terima Kasih!", "Agusse Expedition",
-	                                    JOptionPane.INFORMATION_MESSAGE);
-	                        } else if (choose == 1) {
-	                            isStop = false;
-	                        }
-	                        break;
+                    if(pilihan == JOptionPane.YES_OPTION) {
+                        is_exit_lacak_pesanan = true;
+                        i = 4;
+                    } else {
+                        is_exit_lacak_pesanan = false;
+                        i = 0;
+                    }
 
-	                    default:
-	                        break;
-	                }
-	            } catch (NumberFormatException e) {
-	                JOptionPane.showMessageDialog(null, "Harap masukkan angka!", "Error!", JOptionPane.ERROR_MESSAGE);
-	            }
-	        } while (isStop == false);
-	    }
+                }
 
-	    public static int ongkir(String berat, String jarak) {
-	        int vberat = Integer.parseInt(berat);
-	        int vjarak = Integer.parseInt(jarak);
-	        int totalOngkir = 0;
-	        int hargaBerat = 0;
-	        int hargaJarak = 0;
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Data masih kosong!", "PERINGATAN", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    private void CetakResi() {
+        int berat_barang = Integer.parseInt(pesanan.get(0));
+        int jarak_barang = Integer.parseInt(pesanan.get(1));
 
-	        hargaBerat = (vberat / 100) * 5000;
-	        hargaJarak = (vjarak / 10) * 8000;
+        long total_bayar = KalkulasiPembayaran(berat_barang, jarak_barang);
+        String nomor_resi = "AGS001";
 
-	        totalOngkir = hargaBerat + hargaJarak;
+        pesanan.add(2, ""+total_bayar);
+        pesanan.add(3, nomor_resi);
 
-	        return totalOngkir;
-	    }
+        String list_data = "Berat Barang = " + berat_barang + " gr\n"
+                           + "Jarak Tempuh = " + jarak_barang + " km\n"
+                           + "Total Bayar = Rp " + total_bayar + "\n"
+                           + "Nomor Resi = " + nomor_resi;
+        
+        JOptionPane.showMessageDialog(null, list_data);
+    }
 
-	    public static void cekResi() {
-	        if (order.isEmpty()) {
-	            JOptionPane.showMessageDialog(null, "Data Kosong!", "Cek Resi",
-	                    JOptionPane.ERROR_MESSAGE);
-	        } else {
-	            String no_resi = JOptionPane.showInputDialog(null, "Masukkan No. Resi : ", "Cek Resi",
-	                    JOptionPane.QUESTION_MESSAGE);
-	            if (!no_resi.equals(order.get(3))) {
-	                JOptionPane.showMessageDialog(null, "Data tidak ditemukan!", "Cek Resi",
-	                        JOptionPane.ERROR_MESSAGE);
-	            } else if (no_resi.equals(order.get(3))) {
-	                String output = "";
-	                output += "Status Pengiriman XYZ Expedition\n";
-	                output += "===========================\n\n";
-	                output += "No Resi Pengiriman : " + order.get(3) + "\n";
-	                output += "Total Ongkir : " + order.get(2) + "\n";
-	                output += "Status : Sedang Proses";
-	                JOptionPane.showMessageDialog(null, output, "Cek Resi",
-	                        JOptionPane.INFORMATION_MESSAGE);
-	            }
-	        }
-	    }
+    private long KalkulasiPembayaran(int berat, int jarak) {
+        long harga_barang = 5000;
+        long harga_jarak = 8000;
+
+        long total_harga_barang = berat / 100 * harga_barang;
+        long total_harga_jarak = jarak / 10 * harga_jarak;
+        long total_harga = total_harga_barang + total_harga_jarak;
+
+        return total_harga;
+    }
+
+    private void ExitApps(String values) {
+        int pilihan = JOptionPane.showConfirmDialog(null,
+                      values + "Apakah Anda ingin keluar?",
+                      "Konfirmasi",
+                      JOptionPane.YES_NO_OPTION,
+                      JOptionPane.WARNING_MESSAGE);
+        is_exit = (pilihan == JOptionPane.YES_OPTION) ? true : false;
+    }
 
 }
